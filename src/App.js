85d9a8d53f91b359c72b311961f675cc346fdc59
saqibfaisal <saqibfaisalofficial@gmail.com>
2022-12-29 16:35:30 +0500

    mint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
-
+import mintExampleAbi from "./mintExampleAbi.json";
+import { ethers, BigNumber } from "ethers";
+import { useEffect, useState } from 'react';
+const mintExampleAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+const { ethereum } = window
 function App() {
+  // CONNECTING
+  const [accounts, setAccounts] = useState([]);
+  const [mintAmount, setMintAcount] = useState(1)
+  async function ConnectAccount() {
+    if (window.ethereum) {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      setAccounts(accounts)
+    }
+    else {
+      alert("metamask not install")
+    }
+  }
+  useEffect(() => {
+    ConnectAccount()
+  }, [])
+  async function handleMint() {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        mintExampleAddress,
+        mintExampleAbi.abi,
+        signer
+      )
+      try {
+        const response = await contract.mint(BigNumber.from(mintAmount))
+        console.log("response:", response);
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
+      this is how you crae5te a mint button
+      {
+        accounts.length && (
+          <div>
+            <button onClick={() => setMintAcount(mintAmount - 1)}>-</button>
+            {mintAmount}
+            <button onClick={() => setMintAcount(mintAmount + 1)}>+</button>
+            <button onClick={() => handleMint()}>Mint</button>
+          </div>
+        )
+      }
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +64,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
